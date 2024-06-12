ddfe825b85c84a4ece9d12348f5c347ffa76949d
rawr51919 <colton51919@gmail.com>
2024-06-12 16:00:35 -0300

    add accessible name to search button

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -118,9 +118,9 @@
                 <div class="col-sm-3 col-md-3 pull-right">
                     <form class="navbar-form" role="search">
                         <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Word Search" name="srch-term" id="srch-term">
+                            <input type="text" class="form-control" placeholder="Word Search" name="Search Term" id="srch-term">
                             <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                                <button class="btn btn-default" type="submit" aria-label="Search"><i class="glyphicon glyphicon-search"></i></button>
                             </div>
                         </div>
                     </form>
